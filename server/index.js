ace27c440ec73a1f9ea6316fc171efc890e660c6
Slava Traktor <slavasobb@gmail.com>
2020-09-25 19:08:23 +0300

    multiple chats at one moment

--- server/index.js
+++ server/index.js
@@ -5,13 +5,13 @@ const puppeteer = require("puppeteer");
 const expressWs = require("express-ws");
 const cors = require("cors");
 
+let allChats = [];
+
 (async () => {
   const app = express();
   const wsApp = expressWs(app);
 
   const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto("https://www.twitch.tv/popout/ybicanoooobov/chat");
 
   app.use(cors());
 
@@ -26,9 +26,14 @@ const cors = require("cors");
     res.send(chatContent);
   });
 
-  app.ws('/:nickname', async (ws, req) => {
+  app.ws("/:nickname", async (ws, req) => {
+    const nickname = req.params.nickname;
 
-    console.log(req.params);
+    const isAlreadyOpened = allChats.some((v) => v.nickname === nickname);
+
+    const page = isAlreadyOpened
+      ? getExistingPage(nickname)
+      : await openNewPage(browser, nickname);
 
     const intervalId = setInterval(async () => {
       const content = await page.content();
@@ -39,17 +44,57 @@ const cors = require("cors");
       ws.send(chatContent);
     }, 2000);
 
-    ws.on('close', () => {
+    ws.on("close", () => {
       clearInterval(intervalId);
       console.log(`${intervalId} cleared`);
+
+      closePage(page);
     });
-  })
+  });
 
   app.listen(3000, () => {
     console.log("Started, port: 3000");
   });
 })();
 
+function getExistingPage(nickname) {
+  const chat = allChats.find((v) => v.nickname === nickname);
+  chat.count += 1;
+
+  return chat.page;
+}
+
+async function openNewPage(browser, nickname) {
+  const page = await browser.newPage();
+  await page.goto(`https://www.twitch.tv/popout/${nickname}/chat`);
+
+  allChats = [
+    ...allChats,
+    {
+      page,
+      nickname,
+      count: 1,
+    },
+  ];
+
+  return page;
+}
+
+
+async function closePage(page) {
+  const chat = allChats.find((v) => v.page === page);
+
+  chat.count -= 1;
+
+  console.table(allChats, ['nickname', 'count']);
+
+  if(chat.count !== 0) {
+    return;
+  }
+
+  allChats = allChats.filter(v => v.page !== page);
+  
+}
 // (async () => {
 //   const browser = await puppeteer.launch();
 
